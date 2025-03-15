aef8b0a0bd8c91245c6538ee3786b0840092e26b
vatcharit.o <vatcharit.o@arise.tech>
2025-03-15 14:54:55 +0700

    feat: group to one page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlayerListComponent } from './player-list/player-list.component';
+import { MatchListComponent } from './match-list/match-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PlayerListComponent, MatchListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { PlayerListComponent } from './player-list/player-list.component';
 import { MatchListComponent } from './match-list/match-list.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'player-list', pathMatch: 'full' },
-  { path: 'player-list', component: PlayerListComponent },
-  { path: 'match-list', component: MatchListComponent },
+  // { path: '', redirectTo: 'player-list', pathMatch: 'full' },
+  // { path: 'player-list', component: PlayerListComponent },
+  // { path: 'match-list', component: MatchListComponent },
 ];
