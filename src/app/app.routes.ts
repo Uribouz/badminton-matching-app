d6c89baa35b5a468ebb8bdb44c1fa6bee01b6973
Uribouz <vatcharitopas@gmail.com>
2025-05-25 18:20:19 +0700

    Revert "feat wip: refatoring to matchmaker"
    This reverts commit c8de797c30b3f9e2de0e9eb20cab9e8e6be7728c.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { PlayerListComponent } from './players/player-list/player-list.component';
 import { MatchListComponent } from './matches/match-list/match-list.component';
 import { SettingComponent } from './settings/setting/setting.component';
-import { MatchmakerComponent } from './matchmaker/matchmaker.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'player-list', pathMatch: 'full' },
   { path: 'player-list', component: PlayerListComponent },
   { path: 'match-list', component: MatchListComponent },
-  { path: 'matchmaker', component: MatchmakerComponent},
   { path: 'settings', component: SettingComponent },
 ];
